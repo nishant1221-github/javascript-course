4dafe58270fa4c8f32ff750ae00ef3c63eb000e8
nishant1221-github <nishantsh1221@gmail.com>
2026-08-08 17:24:18 +0000

    operations

---
+++ 01_basics/04_operation.js
@@ -0,0 +1,44 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// Common Operations....
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+// String Cocatenation
+
+// let str1 = "hello"
+// let str2 = " Nishant"
+// let str3 = str1 + str2
+// console.log(str3);
+
+
+console.log("1"+2);
+console.log(1+"2");
+console.log(1+"2"+2);
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(true);   // ==> it will print directly true
+console.log(+true);  // ==> but in this case, it will convert +true into number 1.
+
+
+let num1,num2,num3
+num1=num2=num3=2+2   // ==> no recommended, maintain the readability of the code.
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+++gameCounter
+console.log(gameCounter);
+
+
+
+
+
